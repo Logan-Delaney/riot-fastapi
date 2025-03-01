161014bfa743dce3fcd267f02c62f5d279a3f952
Logan Delaney <logan_delaney@yahoo.com>
2025-02-28 22:51:59 -0500

    add summoner list to match api

--- controllers/match.py
+++ controllers/match.py
@@ -28,7 +28,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 @match_router.get("/match-detail", status_code=status.HTTP_200_OK)
-async def get_matches(puuid: str, match_id: str):
+async def get_matches(puuid: str, match_id: str, db: db_dependency):
     url = f"https://americas.api.riotgames.com/lol/match/v5/matches/{match_id}"
     headers = {
         'X-Riot-Token': api_key
@@ -36,7 +36,19 @@ async def get_matches(puuid: str, match_id: str):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         match = response.json()
-        return match
+        participants = match['info']['participants']
+        summoner_list = []
+        for participant in participants:
+            if participant['puuid'] == puuid:
+                summoner = db.query(models.RiotUser).filter(models.RiotUser.puuid == participant['puuid']).first()
+                summoner_list.append(summoner.game_name)
+            else:
+                game_name_url = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-puuid/{participant['puuid']}"
+                summoner = requests.get(game_name_url, headers=headers).json()
+                summoner_list.append(summoner['gameName'])
+        return {
+            'summoners': summoner_list, 
+            'match': match}
     else:
         raise HTTPException(status_code=response.status_code, detail=response.json())
 
